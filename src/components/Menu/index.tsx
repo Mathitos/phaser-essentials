96fd11086aa1a21768240356ad388cf8ffd32577
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2020-03-12 03:23:04 -0300

    remove all unused stuff

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Menu: React.FC = () => {
+  return <div> Hey this is a menu</div>
+}
+
+export default Menu
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import Menu from './components/Menu'
+import Movimentation from './components/Movimentation'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root: React.FC = () => (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route exact path={'/'} component={Menu} />
+      <Route
+        path={'/movimentation'}
+        component={Movimentation}
+      />
+    </Switch>
+  </BrowserRouter>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Root />, document.getElementById('root'))
